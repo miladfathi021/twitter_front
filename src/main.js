6c1e486dcf5aab5dbfdbaaa5469ec57b7eea0586
Milad <miladfathi021@gmail.com>
2020-09-29 17:33:53 +0330

    Create router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueRouter from 'vue-router';
+import router from "./router/router";
 import store from "./store/store";
-
-Vue.use(VueRouter);
+import "./tools/clickOutside";
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
   store,
 }).$mount('#app')
